5998878f14ccf9da7c2ffeba7a141c20ebe9483a
Lucky472 <lulu@DESKTOP-U5IIEA4>
2024-10-20 22:32:35 +0200

    test commit

--- mod_precision.f90
+++ mod_precision.f90
@@ -4,6 +4,6 @@ module mod_precision
     integer, parameter          :: nb_max_sommets = 5
 
 ! Note : Il faut cfl <= 0.95 pour que le schema V.F converge sur mesh3_3
-    real(kind = pr)             :: cfl = 0.95
+    real(kind = pr)             :: cfl = 1.
 
 end module mod_precision
