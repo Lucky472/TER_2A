21f97b4889dbea19d9af8541c0dbe7c9c26afc2b
Lucky472 <lulu@DESKTOP-U5IIEA4>
2024-10-20 16:04:34 +0200

    a

--- chaleur.f90
+++ chaleur.f90
@@ -42,11 +42,12 @@ program chaleur
     call maillage("TYP2/"//fichier, nb_mailles, nb_aretes, sommets_maille, noeud_maille, coord_noeud &
     &             , aire_maille, l_arete, d_arete, milieu_arete, ar, trig, cl_arete_bord)
 
+
 ! Allocation des tableaux de temperature
     allocate(Tn(1:nb_mailles)) ; allocate(Tnp1(1:nb_mailles))
 ! Initialisation de Tnp1 et Tn
     Tn = Tinit
-    Tnp1 = 0._pr
+    Tnp1 = Tn  
 
 ! Calcul du pas de temps
     dt = 1._pr
@@ -67,6 +68,7 @@ program chaleur
 ! Implementation du schema
     t = 0._pr ; tmax = 1._pr
     n = FLOOR(tmax/dt) + 1
+
     do j = 1, n
 
         do e = 1 , nb_aretes
@@ -74,19 +76,19 @@ program chaleur
 
             if (k == 0) then
                 if (cl_arete_bord(e)==10) then
-                        Fie=-D*(Tg - Tn(i))/d_arete(e)
-                        Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*d_arete(i)*Fie
+                        Fie = -D*(Tg - Tn(i))/d_arete(e)
+                        Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*l_arete(i)*Fie
                 else if (cl_arete_bord(e)==11) then
                         Fie = -D*(Td - Tn(i))/d_arete(e)
-                        Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*d_arete(i)*Fie
+                        Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*l_arete(i)*Fie
                 else if (cl_arete_bord(e)==20) then
-                      Fie = 0._pr
+                        Fie = 0._pr
                 end if
 
             else
                 Fie = -D*(Tn(k) - Tn(i))/d_arete(e)
-                Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*d_arete(e)*Fie
-                Tnp1(k) = Tnp1(k) + (dt/aire_maille(k))*d_arete(e)*Fie
+                Tnp1(i) = Tnp1(i) - (dt/aire_maille(i))*l_arete(e)*Fie
+                Tnp1(k) = Tnp1(k) + (dt/aire_maille(k))*l_arete(e)*Fie
 
             end if
             
@@ -95,7 +97,7 @@ program chaleur
         Tn = Tnp1
 
         nplot = FLOOR(REAL(n)/10)
-        if (MODULO(j, nplot) == 0) then
+        if (j == 1 .or. MODULO(j, nplot) == 0) then
             call sortie(j, Tn, sommets_maille, noeud_maille, coord_noeud)
         end if
 
